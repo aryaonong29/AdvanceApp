0f4f27f26135716db237c53d6d53cb415084f95a
aryaonong29 <aryaonong29@gmail.com>
2016-09-26 22:38:15 +0700

    sql done,fragment not yet

--- app/src/main/java/com/arianasp/advanceapp/activity/DashboardActivity.java
+++ app/src/main/java/com/arianasp/advanceapp/activity/DashboardActivity.java
@@ -2,11 +2,16 @@ package com.arianasp.advanceapp.activity;
 
 import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.arianasp.advanceapp.R;
-import com.arianasp.advanceapp.activity.BaseActivity;
+import com.arianasp.advanceapp.database.ExpensesDataBaseSQLite;
 
 public class DashboardActivity extends BaseActivity {
 
@@ -19,11 +24,116 @@ public class DashboardActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-        RecyclerView recList = (RecyclerView) findViewById(R.id.cardList);
-        recList.setHasFixedSize(true);
-        LinearLayoutManager llm = new LinearLayoutManager(this);
-        llm.setOrientation(LinearLayoutManager.VERTICAL);
-        recList.setLayoutManager(llm);
+
+
+        final ExpensesDataBaseSQLite db = new ExpensesDataBaseSQLite(this);
+        cIncome = db.addIncome();
+        cExpenses = db.addExpenses();
+
+        recyclerViewIncome = (RecyclerView) findViewById(R.id.recyclerViewIncome);
+        recyclerViewIncome.setHasFixedSize(true);
+
+        recyclerViewExpenses = (RecyclerView) findViewById(R.id.recyclerViewExpenses);
+        recyclerViewExpenses.setHasFixedSize(true);
+
+        rvLmIncome = new LinearLayoutManager(this);
+        rvLmExpenses = new LinearLayoutManager(this);
+        recyclerViewIncome.setLayoutManager(rvLmIncome);
+        recyclerViewExpenses.setLayoutManager(rvLmExpenses);
+
+        rvAdapterIncome = new IncomeAdapter(putIncome());
+        recyclerViewIncome.setAdapter(rvAdapterIncome);
+
+        rvAdapterExpenses = new ExpenseAdapter(putExpenses());
+        recyclerViewExpenses.setAdapter(rvAdapterExpenses);
+    }
+
+    private String [] putIncome(){
+        String [] incomeA = new String[cIncome.getCount()];
+        while(cIncome.moveToNext()){
+            incomeA[cIncome.getPosition()] = cIncome.getString(1)+" "+cIncome.getString(2);
+        }
+        return incomeA;
+    }
+
+    public class IncomeAdapter extends RecyclerView.Adapter<IncomeAdapter.ViewHolder>{
+        private String [] dataI;
+
+        public class ViewHolder extends RecyclerView.ViewHolder{
+            public CardView cvIncome;
+            public TextView tvIncome;
+
+            public ViewHolder(View v){
+                super(v);
+                cvIncome = (CardView) v.findViewById(R.id.cvIncome);
+                tvIncome = (TextView) v.findViewById(R.id.tvIncome);
+            }
+        }
+
+        public IncomeAdapter(String [] data){
+            dataI = data;
+        }
+
+        @Override
+        public IncomeAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_income, parent, false);
+            ViewHolder vhdr = new ViewHolder(view);
+            return vhdr;
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            holder.tvIncome.setText(dataI[position]);
+        }
+
+        @Override
+        public int getItemCount() {
+            return dataI.length;
+        }
+    }
+
+    private String[] putExpenses(){
+        String[] expensesA = new String[cExpenses.getCount()];
+        while(cExpenses.moveToNext()){
+            expensesA[cExpenses.getPosition()] = cExpenses.getString(1)+" "+cExpenses.getString(2);
+        }
+        return expensesA;
+    }
+
+    public class ExpenseAdapter extends RecyclerView.Adapter<ExpenseAdapter.ViewHolder>{
+        private String[] dataE;
+
+        public class ViewHolder extends RecyclerView.ViewHolder{
+            public  CardView cvExpenses;
+            public TextView tvExpenses;
+
+            public ViewHolder(View v){
+                super(v);
+                cvExpenses = (CardView) v.findViewById(R.id.cvExpenses);
+                tvExpenses = (TextView) v.findViewById(R.id.tvExpenses);
+            }
+        }
+
+        public ExpenseAdapter(String[] data){
+            dataE = data;
+        }
+
+        @Override
+        public ExpenseAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_expense, parent, false);
+            ViewHolder vhdr = new ViewHolder(v);
+            return vhdr;
+        }
+
+        @Override
+        public void onBindViewHolder(ExpenseAdapter.ViewHolder holder, int position) {
+            holder.tvExpenses.setText(dataE[position]);
+        }
+
+        @Override
+        public int getItemCount() {
+            return dataE.length;
+        }
     }
 
 
--- app/src/main/java/com/arianasp/advanceapp/database/ExpensesDataBaseSQLite.java
+++ app/src/main/java/com/arianasp/advanceapp/database/ExpensesDataBaseSQLite.java
@@ -1,6 +1,8 @@
 package com.arianasp.advanceapp.database;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -19,8 +21,8 @@ public class ExpensesDataBaseSQLite extends SQLiteOpenHelper {
 
     public static final String TABLE_NAME_EXPENSES = "expenses";
     public static final String COL_EX_ID = "ID";
-    public static final String COL_EX_DES = "STUFF";
-    public static final String COL_EX_AMO = "PRICE";
+    public static final String COL_EX_DES = "DESCRIPTION";
+    public static final String COL_EX_PR = "PRICE";
 
     public static final String TABLE_CREATE_INCOME = "CREATE TABLE " + TABLE_NAME_INCOME + " ( " +
             COL_IN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
@@ -30,14 +32,11 @@ public class ExpensesDataBaseSQLite extends SQLiteOpenHelper {
     public static final String TABLE_CREATE_EXPENSES = "CREATE TABLE " + TABLE_NAME_EXPENSES + " ( " +
             COL_EX_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
             COL_EX_DES + " TEXT, " +
-            COL_EX_AMO + " TEXT, ";
+            COL_EX_PR + " TEXT, ";
 
-    public ExpensesDataBaseSQLite(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+    public ExpensesDataBaseSQLite(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
-
-    
-
 
     @Override
     public void onCreate(SQLiteDatabase db) {
@@ -51,6 +50,65 @@ public class ExpensesDataBaseSQLite extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME_EXPENSES);
     }
 
-    //method untuk save data icncome
-    public boolean saveDataIncome(S)
+    //method untuk save data income
+    public boolean saveDataIncome(String desc,String amount){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cVal = new ContentValues();
+        cVal.put(COL_IN_DES, desc);
+        cVal.put(COL_IN_AMO, amount);
+        long temp = db.insert(TABLE_NAME_INCOME,null,cVal);
+        return temp != -1;
+    }
+
+    //method untuk save data expenses
+    public boolean saveDataExpenses(String desc,String price){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cVal = new ContentValues();
+        cVal.put(COL_EX_DES, desc);
+        cVal.put(COL_EX_PR, price);
+        long temp = db.insert(TABLE_NAME_EXPENSES,null,cVal);
+        return temp != -1;
+    }
+
+    public Cursor addIncome() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor income = db.rawQuery("SELECT * FROM " + TABLE_NAME_INCOME, null);
+        return income;
+    }
+
+    public Cursor addExpenses() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor expense = db.rawQuery("SELECT * FROM " + TABLE_NAME_EXPENSES, null);
+        return expense;
+    }
+
+    public boolean updateIncome(String id, String desc, String amount) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cVal = new ContentValues();
+        cVal.put(COL_IN_ID, id);
+        cVal.put(COL_IN_DES, desc);
+        cVal.put(COL_IN_AMO, amount);
+        db.update(TABLE_NAME_INCOME, cVal, "ID = ? ", new String[]{id});
+        return true;
+    }
+
+    public boolean updateExpense(String id, String desc, String price) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cVal = new ContentValues();
+        cVal.put(COL_EX_ID, id);
+        cVal.put(COL_EX_DES, desc);
+        cVal.put(COL_EX_PR, price);
+        db.update(TABLE_NAME_EXPENSES, cVal, "ID = ? ", new String[]{id});
+        return true;
+    }
+
+    public Integer deleteIncome(String id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(TABLE_NAME_INCOME, "ID = ?", new String[] {id});
+    }
+
+    public Integer deleteExpense(String id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(TABLE_NAME_EXPENSES, "ID = ?", new String[] {id});
+    }
 }

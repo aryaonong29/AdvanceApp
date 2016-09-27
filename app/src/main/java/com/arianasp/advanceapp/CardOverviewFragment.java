0d5771cb6cfe68cce2681141f75059b22e38a8b0
arianasp <aryaonong29@gmail.com>
2016-09-27 16:55:21 +0700

    error layouting

--- app/src/main/java/com/arianasp/advanceapp/CardOverviewFragment.java
+++ app/src/main/java/com/arianasp/advanceapp/CardOverviewFragment.java
@@ -23,44 +23,45 @@ public class CardOverviewFragment extends Fragment {
         tvDesription = (EditText)view.findViewById(R.id.tvDescription);
         tvAmount = (EditText)view.findViewById(R.id.tvAmount);
         buttonAddIncome = (Button)view.findViewById(R.id.buttonAddIncome);
-
-
-        buttonAddIncome.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String des = tvDesription.getText().toString();
-                String amo = tvAmount.getText().toString();
-
-                boolean result = db.saveDataIncome(des, amo);
-                if (result) {
-                    Toast.makeText(getActivity(), "Add Income Success", Toast.LENGTH_SHORT).show();
-                    tvDesription.setText("");
-                    tvAmount.setText("");
-                }else {
-                    Toast.makeText(getActivity(), "Add Income Failed", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
         buttonCancelIncome = (Button)view.findViewById(R.id.buttonCancelIncome);
-        buttonAddExpenses.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String des = tvStuff.getText().toString();
-                String amo = tvPrice.getText().toString();
+        if(view.getId() == R.id.buttonAddIncome){
+            buttonAddIncome.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String des = tvDesription.getText().toString();
+                    String amo = tvAmount.getText().toString();
 
-                boolean result = db.saveDataExpenses(des, amo);
-                if (result) {
-                    Toast.makeText(getActivity(), "Add Expenses Success", Toast.LENGTH_SHORT).show();
-                    tvStuff.setText("");
-                    tvPrice.setText("");
-                }else {
-                    Toast.makeText(getActivity(), "Add Expenses Failed", Toast.LENGTH_SHORT).show();
+                    boolean result = db.saveDataIncome(des, amo);
+                    if (result) {
+                        Toast.makeText(getActivity(), "Add Income Success", Toast.LENGTH_SHORT).show();
+                        tvDesription.setText("");
+                        tvAmount.setText("");
+                    }else {
+                        Toast.makeText(getActivity(), "Add Income Failed", Toast.LENGTH_SHORT).show();
+                    }
                 }
-            }
-        });
+            });
+        }
+        else if(view.getId() == R.id.buttonAddExpenses){
+            buttonAddExpenses.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String des = tvStuff.getText().toString();
+                    String amo = tvPrice.getText().toString();
 
-
+                    boolean result = db.saveDataExpenses(des, amo);
+                    if (result) {
+                        Toast.makeText(getActivity(), "Add Expenses Success", Toast.LENGTH_SHORT).show();
+                        tvStuff.setText("");
+                        tvPrice.setText("");
+                    }else {
+                        Toast.makeText(getActivity(), "Add Expenses Failed", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
         return view;
     }
+
+
 }
--- app/src/main/java/com/arianasp/advanceapp/database/DataBaseSQLite.java
+++ app/src/main/java/com/arianasp/advanceapp/database/DataBaseSQLite.java
@@ -29,12 +29,12 @@ public class DataBaseSQLite extends SQLiteOpenHelper {
     public static final String TABLE_CREATE_INCOME = "CREATE TABLE " + TABLE_NAME_INCOME + " ( " +
             COL_IN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
             COL_IN_DES + " TEXT, " +
-            COL_IN_AMO + " TEXT, ";
+            COL_IN_AMO + " TEXT );";
 
     public static final String TABLE_CREATE_EXPENSES = "CREATE TABLE " + TABLE_NAME_EXPENSES + " ( " +
             COL_EX_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
             COL_EX_DES + " TEXT, " +
-            COL_EX_AMO + " TEXT, ";
+            COL_EX_AMO + " TEXT );";
 
     public DataBaseSQLite(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -50,6 +50,7 @@ public class DataBaseSQLite extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME_INCOME);
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME_EXPENSES);
+        onCreate(db);
     }
 
     //method untuk save data income
